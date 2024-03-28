4fccd7f2b1acfb0e19d2a8a2587eee573eb9983b
ManchanayakaMMTS <thushanshanilkaofficial@gmail.com>
2024-03-28 17:38:01 +0530

    Flowbit and DaisyUI Installed

---
+++ client/src/company_side/pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+function Dashboard() {
+  return (
+    <div className="">
+      <div>
+        <h2>Dashboard</h2>
+      </div>
+      <div>
+        <h1>Yooooo</h1>
+        <p>This is the content of thhdjhgkdhfke page.</p>
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import Dashboard from "./company_side/pages/Dashboard/Dashboard.jsx";
+import CompanyDrawer from "./company_side/components/companyDrawer.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Dashboard />
+  </React.StrictMode>
+);
